e553fe89548a5438393d3fd12a2302d3a4008ab4
Akashdeep <akash.kashhyap@gmail.com>
2024-01-10 10:45:25 +0530

    adding hello

--- server/index.js
+++ server/index.js
@@ -43,6 +43,10 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
